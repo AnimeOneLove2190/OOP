f05733fcff2f5916070ca3093e8ed641c8d1e008
animeonelove2190 <daniil.davletshin1@gmail.com>
2023-09-11 01:43:01 +0500

    Добавьте файлы проекта.

---
+++ Quest01/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Quest01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FlatWithAutoProperties FlatAutoProp = new FlatWithAutoProperties();
+            FlatAutoProp.FullName = "Зубенко Михаил Петрович";
+            FlatAutoProp.Id = 0;
+            FlatAutoProp.Type = Type.fiveRooms;
+        }
+    }
+}
---
+++ Quest01/Type.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Quest01
+{
+    enum Type
+    {
+        studio,
+        oneRoom,
+        twoRooms,
+        threeRooms,
+        fourRooms,
+        fiveRooms,
+        other
+    }
+}
