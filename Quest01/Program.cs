e6f0145133f43eb1614d8ba44db9bb2fca575065
animeonelove2190 <daniil.davletshin1@gmail.com>
2023-09-11 22:21:59 +0500

    feature/515

--- Quest01/Program.cs
+++ Quest01/Program.cs
@@ -10,6 +10,17 @@ namespace Quest01
             FlatAutoProp.FullName = "Зубенко Михаил Петрович";
             FlatAutoProp.Id = 0;
             FlatAutoProp.Type = Type.fiveRooms;
+            Employee employeeOne = new Employee();
+            EmployeeService employeeService = new EmployeeService();
+            employeeOne.FullName = "Зубенко Михаил Петрович";
+            employeeOne.Id = 0;
+            employeeOne.Job = JobTitle.mafia;
+            employeeOne.Wage = 22500;
+            employeeOne.DayOfBirth = new DateTime(1983, 07, 18);
+            employeeOne.EmploymentDate = new DateTime(2001, 08, 19);
+            employeeService.GetInfo(employeeOne);
+            int ageOne = employeeService.GetAge(employeeOne);
+            Console.WriteLine(ageOne);
         }
     }
 }
