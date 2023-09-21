f51fc370013fdca721eca506aca55c864dc22e1a
animeonelove2190 <daniil.davletshin1@gmail.com>
2023-09-21 17:28:37 +0500

    feature/530/Transport

---
+++ Quest01/Helicopter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Quest01
+{
+    class Helicopter : Transport
+    {
+        public int Carrying { get; set; }
+        public override string GetInfo()
+        {
+            return $"Мой владелец: {Owner}. Моя скорость: {Speed} км/ч. Моя грузоподъёмность: {Carrying} кг";
+        }
+    }
+}
