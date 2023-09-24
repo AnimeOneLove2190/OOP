6d193e4e485e11d7c72c8e8ea6c3915965b7fc18
animeonelove2190 <daniil.davletshin1@gmail.com>
2023-09-25 01:08:03 +0500

    feature/541/char-string statistic

---
+++ Quest01/ICharable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Quest01
+{
+    interface ICharable
+    {
+        public Dictionary<char, int> GetCharStatistics(string text);
+    }
+}
