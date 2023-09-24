abe3c6013e80b595412ec530ab551eed03928049
animeonelove2190 <daniil.davletshin1@gmail.com>
2023-09-25 01:02:11 +0500

    feature/534/message

---
+++ Quest01/Letter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Quest01
+{
+    class Letter : Message
+    {
+        public string SenderFullName { get; set; }
+        public string RecipientFullName { get; set; }
+        public string SenderAddress { get; set; }
+        public string RecipientAddress { get; set; }
+        public string RecipientPostcode { get; set; }
+        public override string Send()
+        {
+            return $"Письмо {Id} отправлено. Отправитель: {SenderFullName}, {SenderAddress}. Получатель: {RecipientFullName}, {RecipientAddress}, {RecipientPostcode}";
+        }
+    }
+}
