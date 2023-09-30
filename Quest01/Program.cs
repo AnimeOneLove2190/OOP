4c32c253c17e7502c0aa9f1e37bcfef0c2aef75d
animeonelove2190 <daniil.davletshin1@gmail.com>
2023-10-01 02:57:42 +0500

    feature/563/2

--- Quest01/Program.cs
+++ Quest01/Program.cs
@@ -16,87 +16,108 @@ namespace Quest01
     {
         static void Main(string[] args)
         {
-            FlatWithAutoProperties FlatAutoProp = new FlatWithAutoProperties();
-            FlatAutoProp.FullName = "Зубенко Михаил Петрович";
-            FlatAutoProp.Id = 0;
-            FlatAutoProp.Type = Flats.Type.fiveRooms;
-            Employee employeeOne = new Employee();
-            employeeOne.FullName = "Зубенко Михаил Петрович";
-            employeeOne.Id = 0;
-            employeeOne.Job = JobTitle.Mafia;
-            employeeOne.Wage = 22500;
-            employeeOne.DayOfBirth = new DateTime(1983, 07, 18);
-            employeeOne.EmploymentDate = new DateTime(2001, 08, 19);
-            Employee employeeTwo = new Employee();
-            employeeTwo.FullName = "Violet Evergarden";
-            employeeTwo.Id = 1;
-            employeeTwo.Job = JobTitle.RoyalArmySpecialOfficer;
-            employeeTwo.Job = JobTitle.AutoRecordingDoll;
-            employeeTwo.Wage = 63567;
-            employeeTwo.DayOfBirth = new DateTime(2004, 02, 17);
-            employeeTwo.EmploymentDate = new DateTime(2018, 05, 03);
-            Employee employeeThree = new Employee();
-            employeeThree.FullName = "Hanji Zoe";
-            employeeThree.Id = 2;
-            employeeThree.Job = JobTitle.CommanderOfTheParadiseIslandSurveyCorps;
-            employeeThree.Wage = 116116;
-            employeeThree.DayOfBirth = new DateTime(1982, 06, 06);
-            employeeThree.EmploymentDate = new DateTime(2018, 10, 15);
-            List<Employee> employeeList = new List<Employee>();
-            employeeList.Add(employeeOne);
-            employeeList.Add(employeeTwo);
-            employeeList.Add(employeeThree);
-            EmployeeService employeeService = new EmployeeService();
-            List<Employee> employeesMaxWageList = employeeService.GetEmployeesListMaxWage(employeeList);
+            var FlatAutoProp = new FlatWithAutoProperties
+            {
+                FullName = "Зубенко Михаил Петрович",
+                Id = 0,
+                Type = Flats.Type.fiveRooms
+            };
+            var employeeOne = new Employee
+            {
+                FullName = "Зубенко Михаил Петрович",
+                Id = 0,
+                Job = JobTitle.Mafia,
+                Wage = 22500,
+                DayOfBirth = new DateTime(1983, 07, 18),
+                EmploymentDate = new DateTime(2001, 08, 19)
+            };
+            var employeeTwo = new Employee
+            {
+                FullName = "Violet Evergarden",
+                Id = 1,
+                Job = JobTitle.AutoRecordingDoll,
+                Wage = 63567,
+                DayOfBirth = new DateTime(2004, 02, 17),
+                EmploymentDate = new DateTime(2018, 05, 03)
+        };
+            var employeeThree = new Employee
+            {
+                FullName = "Hanji Zoe",
+                Id = 2,
+                Job = JobTitle.CommanderOfTheParadiseIslandSurveyCorps,
+                Wage = 116116,
+                DayOfBirth = new DateTime(1982, 06, 06),
+                EmploymentDate = new DateTime(2018, 10, 15)
+            };
+            var employeeList = new List<Employee>
+            {
+                employeeOne,
+                employeeTwo,
+                employeeThree
+            };
+            var employeeService = new EmployeeService();
+            var employeesMaxWageList = employeeService.GetEmployeesListMaxWage(employeeList);
             employeesMaxWageList[0].GetInfo();
-            List<Employee> employeesMinWageList = employeeService.GetEmployeesListMinWage(employeeList);
+            var employeesMinWageList = employeeService.GetEmployeesListMinWage(employeeList);
             employeesMinWageList[0].GetInfo();
-            List<Employee> employeesMaxAgeList = employeeService.GetEmployeesListMaxAge(employeeList);
+            var employeesMaxAgeList = employeeService.GetEmployeesListMaxAge(employeeList);
             employeesMaxAgeList[0].GetInfo();
-            List<Employee> employeesMinAgeList = employeeService.GetEmployeesListMinAge(employeeList);
+            var employeesMinAgeList = employeeService.GetEmployeesListMinAge(employeeList);
             employeesMinAgeList[0].GetInfo();
             //Feature530
-            Helicopter helicopter = new Helicopter();
-            helicopter.Owner = "Ростех";
-            helicopter.Speed = 250;
-            helicopter.Carrying = 4000;
+            var helicopter = new Helicopter
+            {
+                Owner = "Ростех",
+                Speed = 250,
+                Carrying = 4000
+            };
             Console.WriteLine(helicopter.GetInfo());
-            Car hayachi = new Car();
-            hayachi.Owner = "Давлетшин Даниил";
-            hayachi.Speed = 110;
-            hayachi.Сonsumption = 6;
+            var hayachi = new Car
+            {
+                Owner = "Давлетшин Даниил",
+                Speed = 110,
+                Сonsumption = 6
+            };
             Console.WriteLine(hayachi.GetInfo());
-            Boat meteor = new Boat();
-            meteor.Owner = "Зеленодольский завод имени А. М.Горького";
-            meteor.Speed = 77;
-            meteor.Displacement = 53;
+            var meteor = new Boat
+            {
+                Owner = "Зеленодольский завод имени А. М.Горького",
+                Speed = 77,
+                Displacement = 53
+            };
             Console.WriteLine(meteor.GetInfo());
             //feature531
-            DesktopComputer desktop = new DesktopComputer();
-            desktop.Model = "TopComp LP 111902013";
-            desktop.VideoCard = "AMD Radeon R2";
-            desktop.RAM = 1;
-            desktop.CPU = "AMD E1 6010";
-            desktop.TypeOfShell = TypeOfShell.Vertical;
-            desktop.BodyMaterial = BodyMaterial.Plastic;
+            var desktop = new DesktopComputer
+            {
+                Model = "TopComp LP 111902013",
+                VideoCard = "AMD Radeon R2",
+                RAM = 1,
+                CPU = "AMD E1 6010",
+                TypeOfShell = TypeOfShell.Vertical,
+                BodyMaterial = BodyMaterial.Plastic
+            };
             Console.WriteLine(desktop.GetInfo());
-            Monoblock monoblock = new Monoblock();
-            monoblock.Model = "Lenovo C20-05";
-            monoblock.VideoCard = "AMD Radeon R2";
-            monoblock.RAM = 4;
-            monoblock.CPU = "AMD E1-6010";
-            monoblock.BodyMaterial = BodyMaterial.Plastic;
-            monoblock.ScreenDiagonal = 19.5;
-            monoblock.Webcam = true;
+            var monoblock = new Monoblock
+            {
+                Model = "Lenovo C20-05",
+                VideoCard = "AMD Radeon R2",
+                RAM = 4,
+                CPU = "AMD E1-6010",
+                BodyMaterial = BodyMaterial.Plastic,
+                ScreenDiagonal = 19.5,
+                Webcam = true
+            };
             Console.WriteLine(monoblock.GetInfo());
-            Laptop veteranOfLabourran = new Laptop();
-            veteranOfLabourran.Model = "Lenovo B590";
-            veteranOfLabourran.VideoCard = "NVIDIA GeForce GT 720M";
-            veteranOfLabourran.RAM = 8;
-            veteranOfLabourran.CPU = "Intel(R) Core(TM) i5-3230M";
-            veteranOfLabourran.BodyMaterial = BodyMaterial.Plastic;
-            veteranOfLabourran.Webcam = true;
-            veteranOfLabourran.BatteryCapacity = 202;
+            var veteranOfLabourran = new Laptop
+            {
+                Model = "Lenovo B590",
+                VideoCard = "NVIDIA GeForce GT 720M",
+                RAM = 8,
+                CPU = "Intel(R) Core(TM) i5-3230M",
+                BodyMaterial = BodyMaterial.Plastic,
+                Webcam = true,
+                BatteryCapacity = 202
+            };
             Console.WriteLine(veteranOfLabourran.GetInfo());
             //Feature532
             //feature533
@@ -107,14 +128,14 @@ namespace Quest01
             string nya = "Ня";
             Console.WriteLine(nya);
             //feature541
-            TechnicalService techService = new TechnicalService();
-            TextService textService = new TextService();
-            CharService charService = new CharService();
-            string text = "Эрвин Смит - 13-й Главнокомандующий Разведкорпуса. Рассудительный, умный и уважаемый человек. Несмотря на аналогичную Леви заботу о членах своего отряда, в случае необходимости без колебаний готов пожертвовать ими ради остального человечества. Он также разработал вид военного построения, позволяющего заранее обнаружить находящегося далеко противника. Также на протяжении большой части своей службы в качестве командира отстаивал независимость Разведкорпуса, тем самым спасая его от расформирования.";
-            Dictionary<char, int> charsCount = charService.GetCharStatistics(text);
+            var techService = new TechnicalService();
+            var textService = new TextService();
+            var charService = new CharService();
+            var text = "Эрвин Смит - 13-й Главнокомандующий Разведкорпуса. Рассудительный, умный и уважаемый человек. Несмотря на аналогичную Леви заботу о членах своего отряда, в случае необходимости без колебаний готов пожертвовать ими ради остального человечества. Он также разработал вид военного построения, позволяющего заранее обнаружить находящегося далеко противника. Также на протяжении большой части своей службы в качестве командира отстаивал независимость Разведкорпуса, тем самым спасая его от расформирования.";
+            var charsCount = charService.GetCharStatistics(text);
             techService.WriteDictionary(charsCount);
             //feature542
-            Dictionary<string, int> wordsStatistic = textService.GetWordStatistics(text);
+            var wordsStatistic = textService.GetWordStatistics(text);
             techService.WriteDictionary(wordsStatistic);
         }
     }
