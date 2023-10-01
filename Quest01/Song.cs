27bbc0aa47d18c45ceaa8cb7473029eb82020759
animeonelove2190 <daniil.davletshin1@gmail.com>
2023-10-02 02:40:54 +0500

    feature/561

---
+++ Quest01/Song.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Quest01
+{
+    class Song
+    {
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public int TechDuration { get; set; }
+        public string Duration { get; set; }
+    }
+}
