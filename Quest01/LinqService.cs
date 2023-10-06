291fbf1f4423677882564296279b611873ce10ff
animeonelove2190 <daniil.davletshin1@gmail.com>
2023-10-06 18:05:30 +0500

    feature/565

--- Quest01/LinqService.cs
+++ Quest01/LinqService.cs
@@ -10,5 +10,23 @@ namespace Quest01
 {
     class LinqService
     {
+        public void GetFirstStudentWithName(List<Student> studentList, string searchName)
+        {
+            if (studentList == null || studentList.Count == 0)
+            {
+                throw new Exception("LinqService.GetFirstStudentWithName: один или несколько параметров содержат null");
+            }
+            var firstStudentWithName = studentList.FirstOrDefault(x => x.Name == searchName);
+            if (firstStudentWithName == null)
+            {
+                throw new Exception("Студент с выбранным именем не найден");
+            }
+            else
+            {
+                Console.WriteLine(firstStudentWithName.Name);
+                Console.WriteLine(firstStudentWithName.Age);
+                Console.WriteLine(firstStudentWithName.CountOfFriends);
+            }
+        }
     }
 }
