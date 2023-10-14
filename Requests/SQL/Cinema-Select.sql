875953ab049caf3c9b60f5c42cc1d8c2da7a5f14
animeonelove2190 <daniil.davletshin1@gmail.com>
2023-10-15 04:13:24 +0500

    feature/581/2

--- Requests/SQL/Cinema-Select.sql
+++ Requests/SQL/Cinema-Select.sql
@@ -22,3 +22,45 @@ FROM Movie
 JOIN MovieGenre ON MovieGenre.MovieId = Movie.Id
 JOIN Genre ON MovieGenre.GenreId = Genre.Id
 GROUP BY Movie.Id, Movie.Name, Movie.Description, Movie.Duration
+--580v
+SELECT
+Row.Id AS RowId,
+Row.Number,
+SUM (Place.Capacity) RowCapacity
+FROM Row
+JOIN Place ON Row.Id = Place.RowId
+GROUP BY Row.Id, Row.Number
+SELECT
+Hall.Id AS HallId,
+SUM (Place.Capacity) HallCapacity
+FROM Hall
+JOIN Row ON Row.HallId = Hall.Id
+JOIN Place ON Row.Id = Place.RowId
+GROUP BY Hall.Id
+--580g
+SELECT
+Hall.Id AS HallId,
+Hall.Name AS HallName,
+SUM (Place.Capacity) HallCapacity
+FROM Hall
+JOIN Row ON Row.HallId = Hall.Id
+JOIN Place ON Row.Id = Place.RowId
+GROUP BY Hall.Id, Hall.Name
+--580d
+SELECT
+Movie.Id,
+Movie.Name,
+COUNT (Ticket.Id) AS PurchasedTickets,
+SUM (Ticket.Price) AS TotalAmount
+FROM Movie
+JOIN Session ON Movie.Id = Session.MovieId
+JOIN Ticket ON Session.Id = Ticket.SessionId
+WHERE Ticket.IsSold = 1
+GROUP BY Movie.Id, Movie.Name
+--580e
+SELECT
+Genre.Id,
+Genre.Name,
+Genre.Description
+FROM Genre
+WHERE Genre.Description != '' AND Genre.Description IS NOT NULL
