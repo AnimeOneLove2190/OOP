ffde56f84347c36e927abd66ff7f867bfad56093
animeonelove2190 <daniil.davletshin1@gmail.com>
2023-10-14 20:21:43 +0500

    feature/580/2

--- Requests/SQL/TestDB-Select.sql
+++ Requests/SQL/TestDB-Select.sql
@@ -1,4 +1,4 @@
---580à
+--580a
 SELECT
 Id,
 Name,
@@ -6,20 +6,20 @@ IsRight,
 QuestionId
 FROM PossibleAnswer
 WHERE IsRight = 1
---580á
+--580b
 SELECT
 Id,
 Name,
 Description,
 TestId
 FROM Question
-WHERE Description != ''
---580â
+WHERE Description != '' OR Description IS NOT NULL
+--580v
 SELECT
 FullName
 FROM SomeUser
 ORDER BY FullName
---580ã
+--580g
 SELECT 
 Question.Id AS QuestId,
 Question.Name AS QuestName,
@@ -30,3 +30,70 @@ Course.Name AS CourseName
 FROM Question
 JOIN Test ON Question.TestId = Test.Id
 JOIN Course ON Test.CourseId = Course.Id
+--580d
+SELECT
+SomeUserId,
+RecordAnswer.PossibleAnswerId,
+PossibleAnswer.Name AS PossibleAnswerName,
+PossibleAnswer.IsRight,
+PossibleAnswer.QuestionId,
+Question.Name AS QuestionName,
+Question.TestId,
+Test.Name AS TestName
+FROM RecordAnswer
+JOIN PossibleAnswer ON PossibleAnswerId = PossibleAnswer.Id
+JOIN Question ON PossibleAnswer.QuestionId = Question.Id
+JOIN Test ON Question.TestId = Test.Id
+--580e
+SELECT
+SomeUserId,
+RecordAnswer.PossibleAnswerId,
+PossibleAnswer.Name AS PossibleAnswerName,
+Question.Description,
+PossibleAnswer.QuestionId,
+Question.Name AS QuestionName,
+Question.TestId,
+Test.Name AS TestName
+FROM RecordAnswer
+JOIN PossibleAnswer ON PossibleAnswerId = PossibleAnswer.Id
+JOIN Question ON PossibleAnswer.QuestionId = Question.Id
+JOIN Test ON Question.TestId = Test.Id
+WHERE PossibleAnswer.IsRight = 1
+--580j
+SELECT
+Question.Id AS QuestionId,
+COUNT(RecordAnswer.SomeUserId) AS RightAnswers
+FROM Question
+JOIN PossibleAnswer ON Question.Id = PossibleAnswer.QuestionId
+JOIN RecordAnswer ON PossibleAnswer.Id = RecordAnswer.PossibleAnswerId
+WHERE PossibleAnswer.IsRight = 1
+GROUP BY Question.Id
+SELECT
+Test.Id AS TestId,
+COUNT(RecordAnswer.SomeUserId) AS RightAnswers
+FROM Test
+JOIN Question ON Test.Id = Question.TestId
+JOIN PossibleAnswer ON Question.Id = PossibleAnswer.QuestionId
+JOIN RecordAnswer ON PossibleAnswer.Id = RecordAnswer.PossibleAnswerId
+WHERE PossibleAnswer.IsRight = 1
+GROUP BY Test.Id
+SELECT
+Course.Id AS CourseId,
+COUNT(RecordAnswer.SomeUserId) AS RightAnswers
+FROM Course
+JOIN Test ON Course.Id = Test.CourseId
+JOIN Question ON Test.Id = Question.TestId
+JOIN PossibleAnswer ON Question.Id = PossibleAnswer.QuestionId
+JOIN RecordAnswer ON PossibleAnswer.Id = RecordAnswer.PossibleAnswerId
+WHERE PossibleAnswer.IsRight = 1
+GROUP BY Course.Id
+--580z
+SELECT
+Question.Id AS QuestionId,
+Question.Name AS QuestionName,
+COUNT(RecordAnswer.SomeUserId) AS RigthtAnswers
+FROM Question
+JOIN PossibleAnswer ON PossibleAnswer.QuestionId = Question.Id
+JOIN RecordAnswer ON RecordAnswer.PossibleAnswerId = PossibleAnswer.Id
+WHERE PossibleAnswer.IsRight = 1
+GROUP BY Question.Id, Question.Name
