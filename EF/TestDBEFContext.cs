0e317d85ddf49925f9025c1a837f2f7469ac9032
animeonelove2190 <daniil.davletshin1@gmail.com>
2023-10-18 03:31:10 +0500

    feature/589/1

--- EF/TestDBEFContext.cs
+++ EF/TestDBEFContext.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.Text;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
 
 namespace EF
 {
-    class TestDBEFContext : DbContext
+    public class TestDBEFContext : DbContext
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
