047dc70e0ceb261ece60566679727a718bb6816d
animeonelove2190 <daniil.davletshin1@gmail.com>
2023-10-21 19:11:53 +0500

    feature/595/2

--- EF/Interfaces/IGenreCRUDService.cs
+++ EF/Interfaces/IGenreCRUDService.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using EFVaiaa.DTOCinema;
 
 namespace EFVaiaa.Interfaces
 {
     interface IGenreCRUDService
     {
+        public void Create();
+        public GenreView Get();
+        public List<GenreView> GetList();
+        public void Update();
+        public void Delete();
     }
 }
